a9905e53c483e08038d17e97e95f843388b9692a
Ambar <Ambarpg80@gmail.com>
2023-01-22 22:27:06 -0500

    commit tree 08be2c2081d629b9ce234a6dec795107568f7f37

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, {useState, useEffect } from 'react';
+import { data } from '../mocks/data';
+
+function App(){
+    const [imageRender, setImageRender] = useState([])
+
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => setImageRender(data))
+        },[])
+        if(!imageRender.message){ 
+           return  <p>loading...</p> }
+        else{
+    return(
+        <div>
+            <h1> Welcome to DOG CEO</h1>
+            <img src={imageRender.message } alt="A Random Dog"></img>  
+        </div>
+        
+    )
+        }
+}
+export default App
